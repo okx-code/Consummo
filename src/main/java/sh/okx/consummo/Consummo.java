67e94c2b828ad0cb7d0cd924afa5619cdd28d2a9
okx-code <me@oliverallen.co.uk>
2018-02-14 18:12:17 +0000

    Remove AutoUpdater from onEnable

--- src/main/java/sh/okx/consummo/Consummo.java
+++ src/main/java/sh/okx/consummo/Consummo.java
@@ -1,12 +1,11 @@
 package sh.okx.consummo;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import sh.okx.consummo.update.AutoUpdater;
 
 public class Consummo extends JavaPlugin {
   @Override
   public void onEnable() {
-    new AutoUpdater(this, 17933).schedule(200,
-        a -> System.out.println("Updated to " + a), true);
+    /*new AutoUpdater(this, 0).schedule(5000,
+        a -> System.out.println("Updated to " + a), true);*/
   }
 }
